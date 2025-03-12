bdfe9f3271cee4a47908d8199a1707a32b669873
Youssef Ben Sadik <youssef@bensadik.net>
2025-03-12 13:15:24 +0000

    refactor: move app conponent is a directory

--- src/main.server.ts
+++ src/main.server.ts
@@ -1,6 +1,6 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './common/components/app.component';
-import { config } from "./common/config/server.config";
+import { AppComponent } from './common/components';
+import { config } from './common/config/server.config';
 
 const bootstrap = () => bootstrapApplication(AppComponent, config);
 
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,5 @@
 import { bootstrapApplication } from '@angular/platform-browser';
-import { AppComponent } from './common/components/app.component';
+import { AppComponent } from './common/components';
 import { clientConfig } from './common/config/client.config';
 
 bootstrapApplication(AppComponent, clientConfig)
